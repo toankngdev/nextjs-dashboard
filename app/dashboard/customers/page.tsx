83305bc38693315bd2e452102060cdd70be348be
toankngdev <toankngdev@gmail.com>
2026-04-16 15:12:34 +0700

    Implement Customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import Table from "@/app/ui/customers/table";
+import { Suspense } from "react";
 import { Metadata } from "next";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
